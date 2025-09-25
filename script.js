5159d9846721c6c4be81d7c1f292ee45eb87d95e
Anyaman Banerjee <abnerjeeoffi3108@gmail.com>
2025-09-25 14:04:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -89,7 +89,17 @@ const units = {
   },
   currency: {
     base: 'USD',
-    currencies: ['AED', 'AUD', 'BRL', 'CAD', 'CHF', 'CNY', 'EUR', 'GBP', 'INR', 'JPY', 'USD'],
+    currencies: [
+      'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL',
+      'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHE', 'CHF', 'CHW', 'CLF', 'CLP', 'CNY', 'COP', 'CRC', 'CUC', 'CUP', 'CVE', 'CZK', 'DJF',
+      'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL',
+      'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW',
+      'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK',
+      'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD',
+      'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLE', 'SLL', 'SOS', 'SRD', 'SSP', 'STN', 'SYP', 'SZL', 'THB', 'TJS', 'TMT',
+      'TND', 'TOP', 'TRY', 'TTD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XOF', 'XPF', 'YER',
+      'ZAR', 'ZMW', 'ZWL'
+    ],
     rates: {}
   }
 };
@@ -97,8 +107,23 @@ const units = {
 let currencyDataFetched = false;
 
 const fallbackRates = {
-  'AED': 3.67, 'AUD': 1.50, 'BRL': 5.60, 'CAD': 1.38, 'CHF': 0.86, 'CNY': 7.10,
-  'EUR': 0.92, 'GBP': 0.77, 'INR': 83.50, 'JPY': 145.00, 'USD': 1.00
+  'AED': 3.67, 'AFN': 70.45, 'ALL': 91.20, 'AMD': 387.50, 'ANG': 1.79, 'AOA': 890.00, 'ARS': 970.00, 'AUD': 1.50, 'AWG': 1.80, 'AZN': 1.70,
+  'BAM': 1.80, 'BBD': 2.00, 'BDT': 119.50, 'BGN': 1.80, 'BHD': 0.38, 'BIF': 2880.00, 'BMD': 1.00, 'BND': 1.35, 'BOB': 6.90, 'BRL': 5.60,
+  'BSD': 1.00, 'BTN': 83.50, 'BWP': 13.50, 'BYN': 3.25, 'BZD': 2.00, 'CAD': 1.38, 'CDF': 2800.00, 'CHE': 0.92, 'CHF': 0.86, 'CHW': 0.86,
+  'CLF': 0.034, 'CLP': 950.00, 'CNY': 7.10, 'COP': 4300.00, 'CRC': 510.00, 'CUC': 1.00, 'CUP': 24.00, 'CVE': 101.00, 'CZK': 23.00, 'DJF': 178.00,
+  'DKK': 6.85, 'DOP': 60.00, 'DZD': 134.00, 'EGP': 48.50, 'ERN': 15.00, 'ETB': 57.00, 'EUR': 0.92, 'FJD': 2.25, 'FKP': 0.77, 'GBP': 0.77,
+  'GEL': 2.70, 'GGP': 0.77, 'GHS': 15.50, 'GIP': 0.77, 'GMD': 69.00, 'GNF': 8600.00, 'GTQ': 7.80, 'GYD': 209.00, 'HKD': 7.80, 'HNL': 24.70,
+  'HRK': 7.00, 'HTG': 132.00, 'HUF': 360.00, 'IDR': 15500.00, 'ILS': 3.75, 'IMP': 0.77, 'INR': 83.50, 'IQD': 1310.00, 'IRR': 42000.00, 'ISK': 138.00,
+  'JEP': 0.77, 'JMD': 156.00, 'JOD': 0.71, 'JPY': 145.00, 'KES': 129.00, 'KGS': 88.00, 'KHR': 4100.00, 'KMF': 450.00, 'KPW': 900.00, 'KRW': 1350.00,
+  'KWD': 0.31, 'KYD': 0.83, 'KZT': 480.00, 'LAK': 22000.00, 'LBP': 89500.00, 'LKR': 300.00, 'LRD': 195.00, 'LSL': 18.00, 'LYD': 4.80, 'MAD': 9.90,
+  'MDL': 17.50, 'MGA': 4500.00, 'MKD': 56.00, 'MMK': 2100.00, 'MNT': 3450.00, 'MOP': 8.05, 'MRU': 39.50, 'MUR': 46.50, 'MVR': 15.40, 'MWK': 1730.00,
+  'MXN': 19.50, 'MYR': 4.35, 'MZN': 64.00, 'NAD': 18.00, 'NGN': 1600.00, 'NIO': 36.50, 'NOK': 10.75, 'NPR': 134.00, 'NZD': 1.65, 'OMR': 0.385,
+  'PAB': 1.00, 'PEN': 3.75, 'PGK': 3.90, 'PHP': 56.00, 'PKR': 278.00, 'PLN': 4.00, 'PYG': 7500.00, 'QAR': 3.64, 'RON': 4.60, 'RSD': 109.00,
+  'RUB': 97.00, 'RWF': 1320.00, 'SAR': 3.75, 'SBD': 8.40, 'SCR': 13.50, 'SDG': 620.00, 'SEK': 10.50, 'SGD': 1.35, 'SHP': 0.77, 'SLE': 23.00,
+  'SLL': 22700.00, 'SOS': 570.00, 'SRD': 36.50, 'SSP': 1300.00, 'STN': 22.50, 'SYP': 13000.00, 'SZL': 18.00, 'THB': 33.50, 'TJS': 10.70,
+  'TMT': 3.50, 'TND': 3.10, 'TOP': 2.35, 'TRY': 34.00, 'TTD': 6.80, 'TWD': 32.00, 'TZS': 2700.00, 'UAH': 41.00, 'UGX': 3700.00, 'USD': 1.00,
+  'UYU': 40.50, 'UZS': 12600.00, 'VES': 36.50, 'VND': 25000.00, 'VUV': 119.00, 'WST': 2.75, 'XAF': 600.00, 'XCD': 2.70, 'XOF': 600.00,
+  'XPF': 111.00, 'YER': 250.00, 'ZAR': 18.00, 'ZMW': 27.00, 'ZWL': 322.00
 };
 
 const products = [
